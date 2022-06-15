d07adaf878fb29c9f054eb4ed889f80d77225695
Cosimo Giovanni Negri <cosimogiovanninegri@gmail.com>
2022-06-15 02:04:21 +0200

    Minor refactor in custom Modal component

--- src/layouts/Modal/Modal.js
+++ src/layouts/Modal/Modal.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Container } from "react-bootstrap";
 
 import styles from "layouts/Modal/modal.module.css";
 
-export const Modal = ({ children }) => {
+export const Modal = ({ handleClosing, children }) => {
   return ReactDOM.createPortal(
     <>
-      <div id={styles["overlay"]} />
-      <div id={styles["popup"]}>{children}</div>
+      <div id={styles.overlay} onClick={handleClosing} />
+      <div id={styles.popup}>{children}</div>
     </>,
     document.getElementById("portal")
   );
